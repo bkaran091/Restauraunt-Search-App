a36f286dea18abecaff2c51b330311a4e633b76f
Karan Bajaj <bkaran091@gmail.com>
2020-05-02 16:47:50 +0530

    adding yelp.js for api request(base url)

---
+++ src/api/yelp.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'https://api.yelp.com/v3/businesses' , //route Url
+    headers:{
+        Authorization: 'Bearer FMqWx22rcshFbwnVtMgbCq6EmfMlUlfWJNr75ynrPcy3qJJYMbDXMRZOtauUpMQ1iomDw8biVgkcQYjxiuEw6mjfOs7qzQ2wjoJ54cvV-wVdYLe8SyiCh4TKP96rXnYx'
+    }
+});
