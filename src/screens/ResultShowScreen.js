5e50a7bfd12246c94599184ee93c0e1a739f0cac
Karan Bajaj <bkaran091@gmail.com>
2020-05-04 11:29:59 +0530

    set loader in ResultShowScreen.js

--- src/screens/ResultShowScreen.js
+++ src/screens/ResultShowScreen.js
@@ -1,14 +1,19 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text, StyleSheet, FlatList, Image} from 'react-native';
+import {View, Text, StyleSheet, FlatList, Image, ActivityIndicator} from 'react-native';
 import yelp from "../api/yelp";
 
 const ResultsShowScreen = ({navigation}) => {
     const [result, setResult] = useState(null);
+    const [loading, setLoading] = useState(false);
+
     const id = navigation.getParam('id');
 
     const getResult = async (id) => {
+        setLoading(true);
         const response = await yelp.get(`/${id}`);
         setResult(response.data);
+        setLoading(false);
+
     };
     useEffect(() => {
         getResult(id);
@@ -21,38 +26,44 @@ const ResultsShowScreen = ({navigation}) => {
     return (
 
         <>
-            <Text style={styles.title}>{result.name}</Text>
-            <FlatList
-                data={result.photos}
-                keyExtractor={(photo) => photo}
-                renderItem={({item}) => {
-                    return (
-                        <View style={styles.container}>
-                            <Image source={{uri: item}} style={styles.image}/>
-                        </View>
-                    )
-                }}
+            {loading && <ActivityIndicator
+                color='#bc2b78'
+                size="large"
+
             />
-        </>
-    )
-}
+            }
+                <Text style={styles.title}>{result.name}</Text>
+                <FlatList
+                    data={result.photos}
+                    keyExtractor={(photo) => photo}
+                    renderItem={({item}) => {
+                        return (
+                            <View style={styles.container}>
+                                <Image source={{uri: item}} style={styles.image}/>
+                            </View>
+                        )
+                    }}
+                />
+            </>
+                )
+            }
 
-const styles = StyleSheet.create({
-    image: {
-        height: 200,
-        width: 300,
-        margin: 15,
-        borderRadius: 2,
-        borderWidth:3
-    },
-    title: {
-        fontSize: 17,
-        textAlign: 'center',
-        fontWeight: 'bold',
-    },
-    container:{
-        alignItems:'center',
-    }
-});
+            const styles = StyleSheet.create({
+            image: {
+            height: 200,
+            width: 300,
+            margin: 15,
+            borderRadius: 2,
+            borderWidth: 3
+        },
+            title: {
+            fontSize: 17,
+            textAlign: 'center',
+            fontWeight: 'bold',
+        },
+            container: {
+            alignItems: 'center',
+        }
+        });
 
-export default ResultsShowScreen;
+            export default ResultsShowScreen;
